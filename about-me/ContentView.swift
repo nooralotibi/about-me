c2b9ae634b24ad2e759e32014079a5b9ca954ec4
nooreealotibi <nooreealotibi@gmail.com>
2024-01-29 08:02:00 +0300

    Initial Commit

---
+++ about-me/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  about-me
+//
+//  Created by noor alotibi on 17/07/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ about-me/about_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  about_meApp.swift
+//  about-me
+//
+//  Created by noor alotibi on 17/07/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct about_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

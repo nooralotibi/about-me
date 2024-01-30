59b61317ccd88a3607fc9bdb8f36ce644babdd2c
nooreealotibi <nooreealotibi@gmail.com>
2024-01-30 07:23:15 +0300

    first

---
+++ about-me/Card.swift
@@ -0,0 +1,18 @@
+//
+//  Card.swift
+//  about-me
+//
+//  Created by noor alotibi on 18/07/1445 AH.
+//
+
+import SwiftUI
+
+struct Card: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Card()
+}

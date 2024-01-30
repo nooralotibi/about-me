5f0e8ef62b0749ac6e683610ad316131c8d49344
nooreealotibi <nooreealotibi@gmail.com>
2024-01-30 07:26:32 +0300

    second

--- about-me/Card.swift
+++ about-me/Card.swift
@@ -7,12 +7,22 @@
 
 import SwiftUI
 
-struct Card: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct Card: Identifiable, Equatable {
+    let id: Int
+    let image: Image
+    let title: String
+    let description: String
+    let link: URL
+    let isExpanded: Bool = false
+
+   
+    static func == (lhs: Card, rhs: Card) -> Bool {
+        lhs.id == rhs.id
     }
 }
+var cards = [
+    Card(id: 0, image: Image("Image 1"), title: "About me", description: "Hello, I'm Noor. I recently graduated from computer engineering and have a strong passion for technology and development for iOS devices. I have proficiency in developing mobile applications and experience in programming. I am always eager to learn and grow in the tech industry. I am passionate about using technology to solve real-world problems.", link: URL(string: "mailto:nooreealotibi@gmail.com?")!),
+    Card(id: 1, image: Image("Image"), title: "Skills", description: "Problem solving - Communication - swift - python -  APIs integration.", link: URL(string: "https://www.linkedin.com/in/noor-aljuaid-09558a240/")!),
+    Card(id: 3, image: Image("Image 2"), title: "Projects", description: "Red flag App Developed an AI-powered application for high-accuracy fraud detection in text data, leveraging advanced NLP techniques and machine learning algorithms.", link: URL(string: "https://github.com/nooralotibi?tab=repositories")!)
+]
 
-#Preview {
-    Card()
-}
--- about-me/ContentView.swift
+++ about-me/ContentView.swift
@@ -5,17 +5,75 @@
 //  Created by noor alotibi on 17/07/1445 AH.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var selectedCard: Card? = nil
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text("About me")
+            .font(.largeTitle)
+            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            .frame(maxWidth: .infinity,alignment: .topLeading)
+            .padding()
+        ScrollView(.vertical) {
+            VStack(spacing: 10) {
+                ForEach(cards) { card in
+                    CardView(card: card, isSelected: card.id == selectedCard?.id)
+                        .onTapGesture {
+                            withAnimation {
+                                selectedCard = card == selectedCard ? nil : card
+                            }
+                        }
+                }
+            }
+            .padding()
         }
-        .padding()
+    }
+}
+
+struct CardView: View {
+    let card: Card
+    let isSelected: Bool
+
+    var body: some View {
+        ZStack {
+
+            RoundedRectangle(cornerRadius: 25)
+                .fill(LinearGradient(colors: [Color.purple, Color.blue ], startPoint: .top, endPoint: .bottom))
+
+
+            VStack(alignment: .leading) {
+                card.image
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 220)
+
+                Text(card.title)
+                    .font(.headline)
+                    .fontWeight(.medium)
+                    .frame(maxWidth: .infinity,alignment: .topLeading)
+                  
+                if isSelected {
+                    Text(card.description)
+                        .font(.body)
+                        .padding(.top,-10)
+                    Link(destination: card.link) {
+                    Text("More")
+                        .font(.body)
+                        .foregroundColor(.white)
+                        .padding(.top,-6)
+                          }
+                }
+            }
+            .padding()
+        }
+            .scaleEffect(x: isSelected ?
+                     0.9 : 1, y: isSelected ? 1 : 1)
+        
+           .opacity(isSelected ? 0.9 : 0.8)
     }
 }
 
